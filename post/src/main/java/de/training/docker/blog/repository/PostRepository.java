7d0cf10e0dfc32c4a15a53d4f65c8307f6bb4028
Alen Tolj <alen.tolj@comsysto.com>
2016-12-08 21:47:06 +0100

    Created db accenss.

--- post/src/main/java/de/training/docker/blog/repository/PostRepository.java
+++ post/src/main/java/de/training/docker/blog/repository/PostRepository.java
@@ -3,8 +3,6 @@ package de.training.docker.blog.repository;
 import de.training.docker.blog.repository.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     List<PostEntity> getAllPosts();
 }
