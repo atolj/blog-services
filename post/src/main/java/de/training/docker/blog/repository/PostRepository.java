44f0b5bd021bba057d607420270cfcbcd774f1e4
Alen Tolj <alen.tolj@comsysto.com>
2016-12-10 18:00:41 +0100

    Changed some properties and methods.

--- post/src/main/java/de/training/docker/blog/repository/PostRepository.java
+++ post/src/main/java/de/training/docker/blog/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package de.training.docker.blog.repository;
 
+import de.training.docker.blog.repository.model.AuthorEntity;
 import de.training.docker.blog.repository.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
-    List<PostEntity> getAllPosts();
+    List<PostEntity> getByAuthor(AuthorEntity author);
 }
